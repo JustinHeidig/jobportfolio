27ac6007f1ec0e6f8dd45642a69aab92ca3290fb
JustinHeidig <jqhbiz@gmail.com>
2024-08-10 19:05:30 -0400

    updated hero and nav

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -2,7 +2,7 @@ export default function HeroSection(){
     return (
         <section id = "heroSection" className="hero--section">
             <div className="hero--section--content--box">
-                <div className ="hero-section-content">
+                <div className ="hero-section--content">
                     <p className="section--title">Hey, I'm Justin</p>
                     <h1 className="hero--section--title">
                         <span className="hero--section-title-color">Full Stack</span>{" "}
@@ -16,7 +16,7 @@ export default function HeroSection(){
                 </div>
                 <button className="btn btn-primary">Get In Touch</button>
             </div>
-            <div className="hero--section-img">
+            <div className="hero--section--img">
                 <img src="./img/hero_img.png" alt="Hero Section"/>
             </div>
         </section>
