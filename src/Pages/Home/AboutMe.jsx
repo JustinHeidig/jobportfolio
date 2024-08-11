61232dc3b8181e58173860866ff1f2557465713b
JustinHeidig <jqhbiz@gmail.com>
2024-08-11 00:45:12 -0400

    about me section and css

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,24 @@
+export default function AboutMe(){
+    return (
+        <section id = "About Me" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt = "About Me"/>
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className = "hero--section--content">
+                    <p className="section--title">About</p>
+                    <h1 className="skills--section--heading">
+                    About Me
+                    </h1>
+                    <p className="hero--section-description">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam totam, voluptate praesentium repellat dolorem sit facere cupiditate est nemo, aliquam vel expedita hic aut officia sequi, officiis consequatur culpa!
+                    </p>
+                    <p className="hero--section-description">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam totam, voluptate praesentium repellat dolorem sit facere cupiditate est nemo?
+                    </p>
+
+                </div>
+            </div>
+        </section>
+    )
+}
